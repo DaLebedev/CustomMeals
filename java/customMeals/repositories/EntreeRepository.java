c980c88834ead310fb11cc7e3e16008d6e566f64
DaLebedev <118710225+DaLebedev@users.noreply.github.com>
2024-04-12 20:05:30 -0500

    Delete java/customMeals directory
    Accidentally pushed changes into directory instead of merging

--- java/customMeals/repositories/EntreeRepository.java
+++
@@ -1,8 +0,0 @@
-package customMeals.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import customMeals.beans.Entree;
-
-public interface EntreeRepository extends JpaRepository<Entree, Long> {
-}
--- java/customMeals/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package customMeals.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import customMeals.beans.MealOrder;
-
-public interface OrderRepository extends JpaRepository<MealOrder, Long>{
-
-}
